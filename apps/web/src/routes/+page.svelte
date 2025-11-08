f72f4d6eb9f7fab563bf87f5e0d7da9c5205c32d
skibidi feet <129893548+Tommy3541@users.noreply.github.com>
2025-11-08 17:59:52 +0100

    Initial commit

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let users = $state<unknown>(null);
+
+	onMount(async () => {
+		const response = await fetch('/api/users');
+		users = await response.json();
+	});
+</script>
+
+<div>
+	<pre>
+		{JSON.stringify(users, null, 2)}
+	</pre>
+	<div>
+		<section>
+			<h2>API Status</h2>
+		</section>
+	</div>
+</div>
