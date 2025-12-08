5260d83572801d2e2ab41241edd1f706fcfe2255
tommy <129893548+Tommy3541@users.noreply.github.com>
2025-12-08 19:58:21 +0100

    Update API URL to use PUBLIC_API_URL

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import { PUBLIC_API_URL } from '$env/static/public';
+
   let organization: string | null = null;
 
   onMount(async () => {
-    const res = await fetch(import.meta.env.VITE_API_URL);
+    const res = await fetch(PUBLIC_API_URL);
     const json = await res.json();
     organization = json.organization;
   });
 </script>
-
-<h1>Hello TdA</h1>
-  <p>Organization: {organization}</p>
-<p>test api</p>
-
