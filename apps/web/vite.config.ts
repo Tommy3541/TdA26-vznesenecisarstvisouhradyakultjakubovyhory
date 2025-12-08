41908842ea25c8a50fa8eeeef7d78a350d8d0341
Jakub Štěpánek <jakub14step@seznam.cz>
2025-12-08 21:59:50 +0100

    Add environment variable definition to Vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,11 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		port: 3001,
-		proxy: {
+  plugins: [sveltekit()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  },
+  server: {
+    port: 3001,
+    proxy: {
       "/api": "http://localhost:3000"
     }
-	},
+  }
 });
+
