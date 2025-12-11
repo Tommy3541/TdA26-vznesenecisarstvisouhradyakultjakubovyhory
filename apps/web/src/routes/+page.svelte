5054a769c9bf4cb69ae9f333197d4fb468d85e71
Jakub Štěpánek <jakub14step@seznam.cz>
2025-12-11 15:45:54 +0100

    Update API endpoint in page.svelte
    Fix API endpoint in fetch call to retrieve organization data.

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -5,7 +5,7 @@
   let organization: string | null = null;
 
   onMount(async () => {
-    const res = await fetch('/api/<endpoint>');
+    const res = await fetch('/api');
     const json = await res.json();
     organization = json.organization;
     console.log("Testovací print, organizace: ")
