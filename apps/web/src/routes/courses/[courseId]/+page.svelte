d7ea73d717f381feb1ab47f3560a67d7c5a0b67c
tommy <129893548+Tommy3541@users.noreply.github.com>
2026-01-03 17:39:03 +0100

    Refactor course detail display in Svelte page

--- apps/web/src/routes/courses/[courseId]/+page.svelte
+++ apps/web/src/routes/courses/[courseId]/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
     export let data;
-    // 'data.course' pochází z návratové hodnoty funkce load v +page.ts
 </script>
-
-<h1>Detail kurzu</h1>
-{#if data.course}
-    <section>
-        <h2>{data.course.name || data.course.title}</h2>
-        <p>{data.course.description}</p>
-    </section>
-{:else}
-    <p>Načítání...</p>
-{/if}
+<h1>{data.course.name}</h1>
+<p>{data.course.description}</p>
