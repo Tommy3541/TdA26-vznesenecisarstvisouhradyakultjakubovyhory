00c26a736bf3d1e2feafed42c1bb165712c73958
Jakub Štěpánek <jakub14step@seznam.cz>
2026-01-06 22:03:44 +0100

    odstraneni slozky v /courses -zastarale ID

--- apps/web/src/routes/courses/[courseId]/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    export let data;
-</script>
-<h1>{data.course.name}</h1>
-<p>{data.course.description}</p>
--- apps/web/src/routes/courses/[courseId]/+page.ts
+++
@@ -1,5 +0,0 @@
-export const load = async ({ fetch, params }) => {
-    const res = await fetch(`/api/courses/${params.courseId}`); // Detail kurzu
-    const course = await res.json();
-    return { course };
-};
