39cfa055f05ce5fdcb6bd73292dbfd19192c4812
tommy <129893548+Tommy3541@users.noreply.github.com>
2026-01-07 21:28:16 +0100

    Format load function for better readability

--- apps/web/src/routes/courses/[uuid]/materials/+page.ts
+++ apps/web/src/routes/courses/[uuid]/materials/+page.ts
@@ -1,1 +1,8 @@
-export const load = async ({ fetch, params }) => {  const res = await fetch(/courses/${params.uuid}/materials); if (res.ok) { const materials = await res.json(); return { materials }; } return { materials: [] }; };
+export const load = async ({ fetch, params }) => {  
+  const res = await fetch(/courses/${params.uuid}/materials);
+  if (res.ok) { 
+    const materials = await res.json(); 
+    return { materials }; 
+  } 
+  return { materials: [] }; 
+};
