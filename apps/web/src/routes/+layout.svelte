a1d91292325ba33d0bbd483c4e8ec6957e102449
Darbyk76 <radek.darbujan@gmail.com>
2026-01-07 20:19:30 +0100

    Update +layout.svelte

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children?.()}
