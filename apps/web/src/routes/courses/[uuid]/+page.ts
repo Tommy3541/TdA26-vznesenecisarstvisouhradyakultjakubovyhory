ce81982fbdcf99888881c79d91744941e5db14f7
tommy <129893548+Tommy3541@users.noreply.github.com>
2026-01-07 20:39:52 +0100

    Delete apps/web/src/routes/courses/[uuid]/+page.ts

--- apps/web/src/routes/courses/[uuid]/+page.ts
+++
@@ -1,9 +0,0 @@
-export const load = async ({ fetch, params }) => {
-    // Odstraňte '/api', protože vaše routy jsou přímo v /courses
-    const res = await fetch(`/courses/${params.uuid}/materials`);
-    if (res.ok) {
-        const materials = await res.json();
-        return { materials };
-    }
-    return { materials: [] };
-};
