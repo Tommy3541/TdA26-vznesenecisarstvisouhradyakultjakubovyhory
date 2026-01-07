3d0a19b97e5ddc07a64342f0fab8878ba6959fae
tommy <129893548+Tommy3541@users.noreply.github.com>
2026-01-07 21:36:44 +0100

    Update +page.ts

--- apps/web/src/routes/courses/[uuid]/materials/+page.ts
+++ apps/web/src/routes/courses/[uuid]/materials/+page.ts
@@ -2,14 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
   const res = await fetch(`/courses/${params.uuid}/materials`);
+  const materials = res.ok ? await res.json() : [];
 
-  if (!res.ok) {
-    return { materials: [] };
-  }
-
-  const materials = await res.json();
-
-  return {
-    materials
-  };
+  return { materials };
 };
