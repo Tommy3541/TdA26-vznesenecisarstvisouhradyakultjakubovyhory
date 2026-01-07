18e5ef93c41d22afd27110e136bc6a994012529c
tommy <129893548+Tommy3541@users.noreply.github.com>
2026-01-07 20:33:11 +0100

    Update +server.ts

--- apps/web/src/routes/courses/[uuid]/materials/[materialId]/+server.ts
+++ apps/web/src/routes/courses/[uuid]/materials/[materialId]/+server.ts
@@ -4,22 +4,28 @@ import { db } from '$lib/server/database';
 export async function PUT({ request, params }) {
     const formData = await request.formData();
     const file = formData.get('file') as File;
+    const title = formData.get('title') as string;
 
     const updated = await db.courseMaterial.update({
         where: { id: params.materialId },
         data: {
-            title: formData.get('title') ? String(formData.get('title')) : undefined,
+            title: title || undefined,
             url: (file && file.size > 0) ? file.name : undefined
         }
     });
 
-    // Pro test 'replace file'
-    if (file && file.size > 0) return json(true);
+    // Pokud test nahrává nový soubor, očekává v JSONu hodnotu 'true'
+    if (file && file.size > 0 && !title) {
+        return json(true); //
+    }
 
-    return json({ uuid: updated.id, name: updated.title });
+    return json({
+        uuid: updated.id,
+        name: updated.title
+    });
 }
 
 export async function DELETE({ params }) {
     await db.courseMaterial.delete({ where: { id: params.materialId } });
-    return new Response(null, { status: 204 }); // Status 204 pro úspěšné smazání
+    return new Response(null, { status: 204 });
 }
